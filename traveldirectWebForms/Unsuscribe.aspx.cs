d46491149f7d9dbc34fa7db691ecb2a0c530fc5e
suset espinosa <s.espinosa@traveldirect.co.uk>
2016-05-05 16:28:40 -0400

    Create two dynamic pages, one for a newsletter campaign and the other for unsuscriptions

---
+++ traveldirectWebForms/Unsuscribe.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace traveldirectWebForms
+{
+    public partial class Unsuscribe : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var qs = HttpContext.Current.Request.QueryString;
+            int clientID = 0;
+
+            int.TryParse(qs["cid"], out clientID);
+
+            if (clientID != 0)
+            {
+                using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["campaignsCnnString"].ConnectionString))
+                {
+                    SqlCommand cmd = new SqlCommand(@"UPDATE Clients SET Subscribed = 0 WHERE ID = @cid", conn);
+                    cmd.CommandType = System.Data.CommandType.Text;
+
+                    //parameter
+                    cmd.Parameters.Add(new SqlParameter()
+                    {
+                        ParameterName = "cid",
+                        DbType = System.Data.DbType.Int32,
+                        Value = clientID
+                    });
+
+                    try
+                    {
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (Exception excep)
+                    {
+                        throw new Exception(string.Format("Unsuscription for client with ID {0} failed", clientID), excep);
+                    }
+                    finally
+                    {
+                        if (conn.State == System.Data.ConnectionState.Open)
+                            conn.Close();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ traveldirectWebForms/campaigns/Campaign1.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace traveldirectWebForms.campaigns
+{
+    public partial class Campaign1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var qs = HttpContext.Current.Request.QueryString;
+            int clientID = 0, destinationNumber = 0, campaing = 0;
+
+            int.TryParse(qs["cid"], out clientID);
+            int.TryParse(qs["value"], out destinationNumber);
+            int.TryParse(qs["campaign"], out campaing);
+
+            if (clientID != 0 && destinationNumber != 0 && campaing != 0)
+            {
+                using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["campaignsCnnString"].ConnectionString))
+                {
+                    SqlCommand cmd = new SqlCommand(@"UPDATE ClientCampaign SET CampaignID = @campaing, Replied = 1, ResponseDataKey = @value, ResponseDate = getdate() WHERE ClientID = @cid", conn);
+                    cmd.CommandType = System.Data.CommandType.Text;
+
+                    //parameters
+                    cmd.Parameters.Add(new SqlParameter()
+                    {
+                        ParameterName = "campaing",
+                        DbType = System.Data.DbType.Int16,
+                        Value = campaing
+                    });
+
+                    cmd.Parameters.Add(new SqlParameter()
+                    {
+                        ParameterName = "value",
+                        DbType = System.Data.DbType.Int16,
+                        Value = destinationNumber
+                    });
+
+                    cmd.Parameters.Add(new SqlParameter()
+                    {
+                        ParameterName = "cid",
+                        DbType = System.Data.DbType.Int32,
+                        Value = clientID
+                    });
+
+                    try
+                    {
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (Exception excep)
+                    {
+                        throw new Exception(string.Format("Update campaign info for client with ID {0} failed", clientID), excep);
+                    }
+                    finally
+                    {
+                        if (conn.State == System.Data.ConnectionState.Open)
+                            conn.Close();
+                    }
+                }
+            }
+        }
+    }
+}
